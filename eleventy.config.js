292c65857a1858ec6c2c7158cf0e68fdddbfc0b4
Reinier Ladan <reinier@ladan.email>
2023-02-14 15:18:59 +0100

    Added mtg collection

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 const htmlmin = require("html-minifier");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+const glob = require("glob-promise");
 
 const { DateTime } = require("luxon");
 
 // module import collections
 const {getAllPosts} = require('./config/collections/index.js');
 const {getAllNewsletters} = require('./config/collections/index.js');
+
+// For image processing
+const THUMB = 250;
+const FULL = 1200;
 
 module.exports = function (eleventyConfig) {
 
@@ -44,6 +50,13 @@ module.exports = function (eleventyConfig) {
 
 		// Change the default file encoding for reading/serving files
 		encoding: "utf-8",
+	});
+
+    // Generate MTG images
+    eleventyConfig.on('beforeBuild', async () => {
+		console.log('beforeBuild');
+		await generateImages();
+		console.log('images done');
 	});
 
 	// Passthrough
@@ -99,6 +112,26 @@ module.exports = function (eleventyConfig) {
 		pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
 	}
 
+    eleventyConfig.addCollection('mtgimages', async collectionApi => {
+
+		let files = await glob('./_site/mtg/*.jpeg');
+		//Now filter to non thumb-
+		let images = files.filter(f => {
+			return f.indexOf('./_site/mtg/thumb-') !== 0;
+		});
+
+		let collection = images.map(i => {
+			return {
+				path: i.replace('./_site/mtg/', '/mtg/'),
+				thumbpath: i.replace('./_site/mtg/', '/mtg/thumb-'),
+                title: i.replace('./_site/mtg/', '')
+			}
+		});
+
+		return collection;
+
+	});
+
 	return {
 		dir: {
 			input: "src",
@@ -118,3 +151,31 @@ function htmlminTransform(content, outputPath) {
 	}
 	return content;
 }
+
+async function generateImages() {
+
+    // TODO: write images to _site/mtg
+	let options = {
+		widths: [THUMB,FULL],
+		formats: ['jpeg'],
+        urlPath: "/mtg/",
+        outputDir: "./_site/mtg/",
+		filenameFormat:function(id, src, width, format, options) {
+			let origFilename = src.split('/').pop();
+			//strip off the file type, this could probably be one line of fancier JS
+			let parts = origFilename.split('.');
+			parts.pop();
+			origFilename = parts.join('.');
+
+			if(width === THUMB) return `thumb-${origFilename}.${format}`;
+			else return `${origFilename}.${format}`;
+		}
+	};
+
+	let files = await glob('./mtg/*.{jpg,jpeg,png,gif}');
+	for(const f of files) {
+		console.log('doing f',f);
+		let md = await Image(f, options);
+	};
+
+};
