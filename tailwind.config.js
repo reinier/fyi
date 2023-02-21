6e2158223e09b7dffaf92bdd46bd0ac14093f1d7
Reinier Ladan <reinier@ladan.email>
2023-02-21 20:39:38 +0100

    New colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-	content: ["./src/**/*.{html,njk,md}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				'publicsans': ['Public Sans webfont', '-apple-system', 'Helvetica Neue', 'Helvetica', 'sans-serif']
-			},
-			colors: {
-        		'fyi-dark-green': '#0d6239',
-        		'fyi-light-green': '#ddfff7'
-      		}
-		},
-	},
-	plugins: [],
+    content: ["./src/**/*.{html,njk,md}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'publicsans': ['Public Sans webfont', '-apple-system', 'Helvetica Neue', 'Helvetica', 'sans-serif']
+            },
+            colors: {
+                'fyi-darkest-tint': '#36244f',
+                'fyi-dark-tint': '#3a579a',
+                'fyi-light-tint': '#faba61',
+                'fyi-alt-tint': '#ff8172',
+                'fyi-bright-tint': '#ff2fa9',
+                'fyi-brightest-tint': '#fffc40'
+            }
+        },
+    },
+    plugins: [],
 };
