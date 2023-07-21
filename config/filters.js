5ea3f625289d81d5d9f66f22d14d2346c091041b
Reinier Ladan <reinierl@gmail.com>
2023-07-21 15:54:21 +0200

    Added thumbs and improved recipe overview page

--- config/filters.js
+++ config/filters.js
@@ -1,70 +1,95 @@
 const { DateTime } = require("luxon");
 
 module.exports = eleventyConfig => {
-    
+
     eleventyConfig.addFilter("readablePostDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj, {
             zone: "Europe/Amsterdam"
         }).setLocale('nl-NL').toFormat('d MMMM, yyyy');
     });
-    
+
     eleventyConfig.addFilter("readablePostDateTime", (dateObj) => {
         return DateTime.fromJSDate(dateObj, {
             zone: "Europe/Amsterdam"
         }).setLocale('nl-NL').toFormat('d MMMM, yyyy HH:mm');
     });
-    
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj, {
             zone: "Europe/Amsterdam"
         }).setLocale('en').toISODate();
     });
-    
+
     eleventyConfig.addFilter("urlHostname", (data) => {
         var url = new URL(data);
-        if(url.hostname.substring(0,4) == 'www.'){
+        if (url.hostname.substring(0, 4) == 'www.') {
             var hostname = url.hostname.substring(4);
         } else {
             var hostname = url.hostname;
         }
         return hostname;
     });
-    
-    eleventyConfig.addFilter("limit", function(array, limit) {
+
+    eleventyConfig.addFilter("limit", function (array, limit) {
         return array.slice(0, limit);
     });
-    
+
     // splitlines for social image
-    eleventyConfig.addFilter('splitlines', function(input) {
-        let lineWidth = 32;        
+    eleventyConfig.addFilter('splitlines', function (input) {
+        let lineWidth = 32;
         return wrapContentToWidth(input, lineWidth);
     });
 
     // splitlines for social image posts
-    eleventyConfig.addFilter('splitlinespost', function(input) {
-        let lineWidth = 28;        
+    eleventyConfig.addFilter('splitlinespost', function (input) {
+        let lineWidth = 28;
         return wrapContentToWidth(input, lineWidth);
+    });
+
+    eleventyConfig.addFilter('getthumb', function (input) {
+        return getThumbFromImageUrl(input);
+    });
+
+    eleventyConfig.addFilter('capitalizeFirstLetter', function (input) {
+        return input.charAt(0).toUpperCase() + input.slice(1);
     });
 };
 
-function wrapContentToWidth(content, lineWidth){
+function getThumbFromImageUrl(imageUrl) {
+
+    const prefix = 'thumb-';
+    const lastIndex = imageUrl.lastIndexOf('/');
+
+    // Extract the directory path and file name
+    const directoryPath = imageUrl.slice(0, lastIndex + 1);
+    const fileName = imageUrl.slice(lastIndex + 1);
+
+    // Combine the prefix and file name to get the final result
+    const finalString = directoryPath + prefix + fileName;
+
+    return finalString;
+
+
+}
+
+function wrapContentToWidth(content, lineWidth) {
     const parts = content.split(' ');
-    const lines = parts.reduce(function(prev, current) {
-        
+    const lines = parts.reduce(function (prev, current) {
+
         if (!prev.length) {
             return [current];
         }
-        
+
         let lastOne = prev[prev.length - 1];
-        
+
         if (lastOne.length + current.length > lineWidth) {
             return [...prev, current];
         }
-        
+
         prev[prev.length - 1] = lastOne + ' ' + current;
-        
+
         return prev;
     }, []);
-    
+
     return lines;
 }
--- config/images.js
+++ config/images.js
@@ -4,7 +4,7 @@ const glob = require("glob-promise");
 const Image = require("@11ty/eleventy-img");
 
 // For image processing
-const THUMB = 250;
+const THUMB = 400;
 const FULL = 1200;
 const QUALITY = 95;
 
@@ -16,7 +16,7 @@ async function generateImages() {
         const subDir = path.dirname(f).replace(/^\.\/src\/images/, "");
 
         let processImage = await Image(f, {
-            widths: [FULL],
+            widths: [THUMB, FULL],
             formats: ['auto'],
             urlPath: "/images/" + subDir,
             outputDir: "./_site/images/" + subDir,
@@ -25,10 +25,69 @@ async function generateImages() {
                 progressive: false
             },
             filenameFormat: function (id, src, width, format, options) {
-                return path.basename(src);
+                if (format == 'jpeg') {
+                    format = 'jpg';
+                }
+                // return path.basename(src);
+                let origFilename = src.split('/').pop();
+                //strip off the file type, this could probably be one line of fancier JS
+                let parts = origFilename.split('.');
+                parts.pop();
+                origFilename = parts.join('.');
+
+                if (width === THUMB) return `thumb-${origFilename}.${format}`;
+                else return `${origFilename}.${format}`;
             }
         });
     };
+};
+
+// async function generateThumbs() {
+
+//     let files = await glob('./src/images/**/*.{jpg,jpeg,png}');
+//     for (const f of files) {
+//         console.log('process thumb: ', f);
+//         const subDir = path.dirname(f).replace(/^\.\/src\/images/, "");
+
+//         let processImage = await Image(f, {
+//             widths: [400],
+//             formats: ['auto'],
+//             urlPath: "/images/" + subDir,
+//             outputDir: "./_site/images/" + subDir,
+//             sharpJpegOptions: {
+//                 quality: QUALITY,
+//                 progressive: false
+//             },
+//             filenameFormat: function (id, src, width, format, options) {
+//                 return path.basename(src);
+//             }
+//         });
+//     };
+// };
+
+async function generateSocialImages() {
+    const socialPreviewImagesDir = "_site/images/social-preview-images/";
+    fs.readdir(socialPreviewImagesDir, function (err, files) {
+        if (files.length > 0) {
+            files.forEach(function (filename) {
+                if (filename.endsWith(".svg")) {
+                    let imageUrl = socialPreviewImagesDir + filename;
+                    Image(imageUrl, {
+                        formats: ["jpeg"],
+                        outputDir: "./" + socialPreviewImagesDir,
+                        sharpJpegOptions: {
+                            quality: QUALITY,
+                            progressive: false
+                        },
+                        filenameFormat: function (id, src, width, format, options) {
+                            let outputFilename = filename.substring(0, (filename.length - 4));
+                            return `${outputFilename}.${format}`;
+                        }
+                    });
+                }
+            })
+        }
+    })
 };
 
 module.exports = eleventyConfig => {
@@ -36,32 +95,12 @@ module.exports = eleventyConfig => {
     eleventyConfig.on('beforeBuild', async () => {
         console.log('beforeBuild');
         await generateImages();
+        // await generateThumbs();
         console.log('images done');
     });
 
     // Social images
-    eleventyConfig.on('afterBuild', () => {
-        const socialPreviewImagesDir = "_site/images/social-preview-images/";
-        fs.readdir(socialPreviewImagesDir, function (err, files) {
-            if (files.length > 0) {
-                files.forEach(function (filename) {
-                    if (filename.endsWith(".svg")) {
-                        let imageUrl = socialPreviewImagesDir + filename;
-                        Image(imageUrl, {
-                            formats: ["jpeg"],
-                            outputDir: "./" + socialPreviewImagesDir,
-                            sharpJpegOptions: {
-                                quality: QUALITY,
-                                progressive: false
-                            },
-                            filenameFormat: function (id, src, width, format, options) {
-                                let outputFilename = filename.substring(0, (filename.length - 4));
-                                return `${outputFilename}.${format}`;
-                            }
-                        });
-                    }
-                })
-            }
-        })
+    eleventyConfig.on('afterBuild', async () => {
+        await generateSocialImages();
     });
 };
