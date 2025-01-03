ff74ca2607d363897924150aaff3834512414d5d
Reinier Ladan <6823+reinier@users.noreply.github.com>
2025-01-03 10:26:49 +0000

    11ty upgraded to version 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,21 @@
-module.exports = ({ env }) => ({
-	plugins: {
-		"postcss-import": {},
-		"tailwindcss/nesting": {},
-		tailwindcss: {},
-		autoprefixer: {},
-		cssnano:
-			env === "production"
-				? {
-					preset: ["default", { discardComments: { removeAll: true } }],
-				}
-				: false,
-	},
-});
+import postcssImport from 'postcss-import';
+import tailwindNesting from "tailwindcss/nesting/index.js";
+import tailwindCss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import cssNano from 'cssnano';
+
+export default function ({ env }) {
+    return {
+        plugins: [
+            postcssImport(),
+            tailwindNesting(),
+            tailwindCss(),
+            autoprefixer(),
+            env === 'production'
+                ? cssNano({
+                    preset: ['default', { discardComments: { removeAll: true } }],
+                })
+                : false,
+        ],
+    };
+};
